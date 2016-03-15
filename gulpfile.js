32557e55a557ac6e1d09460b1bdcdb8e0605cb94
Oleksii An <Oleksii_An@epam.com>
2016-03-15 17:52:39 +0200

    Setting up #1

--- gulpfile.js
+++ gulpfile.js
@@ -4,5 +4,5 @@ var requireDir = require('require-dir');
 requireDir('./gulp', {recurse: true});
 
 gulp.task('build', ['copy', 'sass', 'compress']);
-gulp.task('default', ['copy', 'sass', 'compress', 'watch']);
+gulp.task('default', ['build', 'watch']);
 gulp.task('app', ['build', 'views', 'express', 'watchServer', 'start']);
