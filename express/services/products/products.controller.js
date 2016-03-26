891831f950af5a768bc37dbfaadc534782ac1d04
Oleksii An <voodoo.spr@gmail.com>
2016-03-26 18:31:50 +0200

    Refactored products controller

--- express/services/products/products.controller.js
+++ express/services/products/products.controller.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Product = require('./product.model.js');
+const bindAll = require('lodash.bindall');
 const parseForm = require('./../../helpers/multipartyPromise/multipartyPromise.js');
 const cloudinaryAPI = require('./../../helpers/cloudinaryUploaderPromise/cloudinaryUploaderPromise.js');
 const path = require('path');
@@ -8,61 +9,107 @@ const config = require('./../../config/index');
 const root = require('app-root-path');
 
 module.exports = class ProductController {
-    constructor() {}
+    constructor() {
+        this.req = null;
+        this.res = null;
+        this.next = null;
+
+        bindAll(this, '_sendRes', '_uploadStatic', '_uploadFiles', '_destroyFile', '_saveProduct', '_updateProduct', '_removeProduct');
+    }
 
     get(req, res, next) {
-        Product.find().then(items => {
-            res.json(items);
-        });
+        this.res = res;
+
+        Product.find()
+            .then(this._sendRes)
+            .catch(err => {
+                console.log(err);
+            });
     }
 
     post(req, res, next) {
-        this._serveMultipartForm(req, config.imagePath).then(data => {
-            let product;
+        this.res = res;
 
-            data = this._prepareData(data);
-
-            product = new Product(data);
-
-            product.save().then(data => {
-                res.json(data);
+        this._parseForm(req, config.imagePath)
+            .then(this._uploadStatic)
+            .then(this._saveProduct)
+            .then(this._sendRes)
+            .catch(err => {
+                console.log(err);
             });
-        });
     }
 
     put(req, res, next) {
-        this._serveMultipartForm(req, config.imagePath).then(data => {
-            data = this._prepareData(data);
+        this.res = res;
 
-            Product.update({_id: data._id}, data).then(data => {
-                res.json(data);
+        this._parseForm(req, config.imagePath)
+            .then(this._uploadStatic)
+            .then(this._updateProduct)
+            .then(this._sendRes)
+            .catch(err => {
+                console.log(err);
             });
-        });
     }
 
     delete(req, res, next) {
-        Product.findOne({_id: req.params.id}).then(product => {
-            cloudinaryAPI.delete(product.image.public_id).then(() => {
-                Product.remove({_id: req.params.id}).then(data => {
-                    res.json(data);
-                });
+        this.res = res;
+
+        Product.findOne({_id: req.params.id})
+            .then(this._destroyFile)
+            .then(this._removeProduct)
+            .then(this._sendRes)
+            .catch(err => {
+                console.log(err);
+            });
+    }
+
+    _sendRes(data) {
+        this.res.send(data);
+    }
+
+    _saveProduct(data) {
+        let product;
+
+        data = this._prepareData(data);
+
+        product = new Product(data);
+
+        return product.save();
+    }
+
+    _updateProduct(data) {
+        data = this._prepareData(data);
+
+        return Product.update({_id: data._id}, data);
+    }
+
+    _removeProduct(product) {
+        return Product.remove({_id: product._id});
+    }
+
+    _parseForm(req) {
+        return new Promise((resolve, reject) => {
+            parseForm(req).then(parsed => {
+                let data = {
+                    parsed: parsed,
+                    formData: Object.assign({}, parsed.fields)
+                };
+
+                resolve(data);
             });
         });
     }
 
-    _serveMultipartForm(req) {
+    _uploadStatic(data) {
+        let uploadPromises = [];
+
+        Object.keys(data.parsed.files).forEach(name => {
+            uploadPromises.push(Promise.all(this._uploadFiles(data.parsed, name, data.formData)));
+        });
+
         return new Promise((resolve, reject) => {
-            parseForm(req).then(data => {
-                let formData = Object.assign({}, data.fields);
-                let uploadPromises = [];
-
-                Object.keys(data.files).forEach(name => {
-                    uploadPromises.push(Promise.all(this._uploadFiles(data, name, formData)));
-                });
-
-                Promise.all(uploadPromises).then(results => {
-                    resolve(formData);
-                });
+            Promise.all(uploadPromises).then(() => {
+                resolve(data.formData);
             });
         });
     }
@@ -77,6 +124,14 @@ module.exports = class ProductController {
         });
 
         return promises;
+    }
+
+    _destroyFile(product) {
+        return new Promise((resolve, reject) => {
+            cloudinaryAPI.delete(product.image.public_id).then(() => {
+                resolve(product);
+            });
+        });
     }
 
     _uploadSingeFile(formData, name, file) {
